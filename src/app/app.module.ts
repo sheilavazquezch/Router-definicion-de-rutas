ee9bd469fc7b6ca818d26c56452bfac50727e2be
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-26 01:14:03 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { JuegodadosComponent } from './juegodados/juegodados.component';
+import { DadoComponent } from './dado/dado.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegodadosComponent,
+    DadoComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
